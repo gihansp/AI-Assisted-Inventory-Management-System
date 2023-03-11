30930dfab075e7405d9738a4c736b4a135be52b3
gihan-pathirage <roxannfickes@gmail.com>
2023-03-11 16:42:19 +0530

    home page

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
-# AI-Assisted-Inventory-Management-System
-Our aim is to develop a highly efficient and effective inventory management system, leveraging the power of artificial intelligence.
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Phoenix Industries</title>
+
+	<!-- bootstrap -->
+    <link rel="stylesheet" href="https://bootswatch.com/3/yeti/bootstrap.css">
+
+    <!-- jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <!-- jQuery UI CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/jquery-ui.min.css">
+    <!-- jQuery UI JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="https://bootswatch.com/3/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+  <!-- custom css -->
+  <link rel="stylesheet" href="assets/css/custom.css">
+
+  <!-- jquery -->
+	<script src="assets/jquery/jquery.min.js"></script>
+  <!-- jquery ui -->  
+  <link rel="stylesheet" href="assets/jquery-ui/jquery-ui.min.css">
+  <script src="assets/jquery-ui/jquery-ui.min.js"></script>
+
+  <!-- bootstrap js -->
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</head>
