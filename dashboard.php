55656636c6064b065b4447c7b01fb39ceb2224bd
gihan-pathirage <roxannfickes@gmail.com>
2023-03-26 16:11:22 +0530

    dashboard complete

--- dashboard.php
+++ dashboard.php
@@ -93,4 +93,104 @@
     </div>
 
     <br>
+
+<div class="row">
+
+
+                <div class="col-lg-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Staff Referrals</div>
+                        <div class="panel-body">
+
+
+                            <?php if (isset($_SESSION['userId']) && $_SESSION['userId'] == 1) { ?>
+
+                            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+                            <canvas id="user-orders-chart"></canvas>
+                            <script>
+                                var chartData = '<?php echo $chartDataJson; ?>';
+
+                                // Parse the chart data JSON string into a JavaScript object
+                                chartData = JSON.parse(chartData);
+
+                                // Get the canvas element and create a new Chart.js instance
+                                var ctx = document.getElementById('user-orders-chart').getContext('2d');
+                                var chart = new Chart(ctx, {
+                                    type: 'pie',
+                                    data: {
+                                        labels: Object.keys(chartData),
+                                        datasets: [{
+                                            data: Object.values(chartData),
+                                            backgroundColor: [
+                                                '#FF6384',
+                                                '#36A2EB',
+                                                '#FFCE56',
+                                                '#8E44AD',
+                                                '#3498DB',
+                                                '#27AE60',
+                                                '#F1C40F'
+                                            ]
+                                        }]
+                                    },
+                                    options: {
+                                        title: {
+                                            display: true,
+                                            text: 'userOrders'
+                                        }
+                                    }
+                                });
+                            </script>
+
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+            <?php } ?>
+
+
+
+
+
+            <?php if (isset($_SESSION['userId']) && $_SESSION['userId'] == 1) { ?>
+                <div class="col-lg-8">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Recent Customer Orders</div>
+                        <div class="panel-body">
+                            <table class="table table-striped table-bordered" id="prod_tbl">
+                                <thead>
+                                <tr>
+                                    <th style="width:20%;">Date</th>
+                                    <th style="width:30%;">Name</th>
+                                    <th style="width:20%;">Sales Revenue</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                while ($orderResult = $recentOrdersQuery->fetch_assoc()) {
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $orderResult['date_ordered']; ?></td>
+                                        <td><?php echo $orderResult['customer_name']; ?></td>
+                                        <td><?php echo $orderResult['total_amount_payable']; ?> LKR</td>
+                                    </tr>
+                                    <?php
+                                }
+                                ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+            <?php } ?>
+
+        </div>
+
+</div>
+    
             <?php require_once 'includes/footer.php'; ?>
