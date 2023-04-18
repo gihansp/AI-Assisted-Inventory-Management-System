ffd3e989c331dfb5d54adf1d28c1c2031cc4f013
gihan-pathirage <tharindah4@gmail.com>
2023-04-18 07:58:33 +0530

    editing category

---
+++ functions/category/edit-category.php
@@ -0,0 +1,41 @@
+<?php
+require_once '../session-check.php';
+
+$response = [
+    'isSuccessful' => false,
+    'updateFeedback' => [],
+];
+
+try {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $brandName = filter_input(INPUT_POST, 'mdfy_cats_name', FILTER_SANITIZE_STRING);
+        $categoriesId = filter_input(INPUT_POST, 'mdfy_cats_id', FILTER_SANITIZE_NUMBER_INT);
+
+        if ($brandName !== null && $categoriesId !== null) {
+            $stmt = $connect->prepare('UPDATE categories SET cat_name = ? WHERE cat_id = ?');
+            $stmt->bind_param('si', $brandName, $categoriesId);
+
+            if ($stmt->execute()) {
+                $response['isSuccessful'] = true;
+                $response['updateFeedback'] = 'The categories were updated successfully.';
+            } else {
+                $response['updateFeedback'] = 'An error occurred while updating the categories. Please try again later.';
+            }
+
+            $stmt->close();
+        } else {
+            $response['updateFeedback'] = 'Invalid input.';
+        }
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+
+} catch (Exception $e) {
+    $response['updateFeedback'] = $e->getMessage();
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+$connect->close();
