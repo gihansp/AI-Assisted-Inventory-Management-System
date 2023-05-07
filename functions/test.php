82ee3e2613a0c3b6a33bf6131302da0ed3249c09
gihan-pathirage <roxannfickes@gmail.com>
2023-05-07 23:59:06 +0530

    Create test.php

---
+++ functions/test.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '../session-check.php';
+
+$response = array('isSuccessful' => false, 'updateFeedback' => array());
+
+if (isset($_POST['categoriesId'])) {
+
+
+}
+
+
+GIT_AUTHOR_DATE=$(date -d'2023-03-11 10:15:23') GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE" git commit -m 'commit'
+
